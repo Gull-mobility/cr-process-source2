2d21aeedb798e7dc0c8dfb748f1a329c523d06d9
Alberto Gonzalez <alberto.gonzalez.gomez@hotmail.com>
2022-08-09 18:47:23 +0200

    Add logs in bigquery_positions_by_id

--- bigquery.py
+++ bigquery.py
@@ -6,24 +6,26 @@ client = bigquery.Client()
 #Get actual positions bigquery for uoid
 def bigquery_positions_by_id(uoid):
 
-  query = ' '.join(("SELECT * FROM `vacio-276411.mainDataset.bulkData_b`"
-            " WHERE uoid = '" + uoid +"'",
-            "ORDER BY realTime DESC"))
+    print('Query uoid:"' + uoid +'"')
 
-  query_job = client.query(query)  # Make an API request.
+    query = ' '.join(("SELECT * FROM `vacio-276411.mainDataset.bulkData_b`"
+                " WHERE uoid = '" + uoid +"'",
+                "ORDER BY realTime DESC"))
 
-  vehicles = {}
-  for row in query_job:
-    dictRow = dict(row)
+    query_job = client.query(query)  # Make an API request.
 
-    #Fix some campos that makes problems in fierstore format
-    dictRow['energia'] = float(dictRow['energia'])
-    dictRow['latitud'] = float(dictRow['latitud'])
-    dictRow['longitud'] = float(dictRow['longitud'])
-    dictRow['epochTime'] = float(dictRow['epochTime'])
-    dictRow['autonomyValue'] = float(dictRow['autonomyValue'])
-    dictRow['seats'] = float(dictRow['seats'])
+    vehicles = {}
+    for row in query_job:
+        dictRow = dict(row)
 
-    vehicles[dictRow['matricula']] = dictRow
+        #Fix some campos that makes problems in fierstore format
+        dictRow['energia'] = float(dictRow['energia'])
+        dictRow['latitud'] = float(dictRow['latitud'])
+        dictRow['longitud'] = float(dictRow['longitud'])
+        dictRow['epochTime'] = float(dictRow['epochTime'])
+        dictRow['autonomyValue'] = float(dictRow['autonomyValue'])
+        dictRow['seats'] = float(dictRow['seats'])
 
-  return vehicles
+        vehicles[dictRow['matricula']] = dictRow
+
+    return vehicles
