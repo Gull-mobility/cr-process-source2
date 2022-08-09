d3e1155f7d7ffbe305a62daed82e8a9b7404263e
Alberto Gonzalez <alberto.gonzalez.gomez@hotmail.com>
2022-08-09 18:56:46 +0200

    Fix variable names error.

--- process.py
+++ process.py
@@ -5,13 +5,13 @@ def execution(uoid):
     from movements_analyze import calculate_movements
 
     #Get last database locations
-    dict_old_location = firestore_get_old_situation()
-    print('Firestore has ' + str(len(dict_old_location)) + ' vehicles')
+    old_location = firestore_get_old_situation()
+    print('Firestore has ' + str(len(old_location)) + ' vehicles')
 
     #Get new positions
     new_locations = bigquery_positions_by_id(uoid)
     #Calculate movements
-    changes = calculate_movements(new_locations)
+    changes = calculate_movements(new_locations, old_location)
 
     #Write in firestore the changes
     firestore_write_changes(changes)
